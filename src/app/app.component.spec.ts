ea6e70ddfb9b4f4c0464a9ad99c432ff33c98f27
Jimmy Muchachasoy Muchavisoy <jimmy.muchachasoy@sofka.com.co>
2021-10-28 11:38:24 -0500

    version inicial de la aplicacion demo web

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'demoweb'`, () => {
+  it(`should have as title 'Proteccion'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('demoweb');
+    expect(app.title).toEqual('Proteccion');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('demoweb app is running!');
+
+    expect(compiled.querySelector('h1')?.textContent).toContain('Saludos Equipo !!!');
   });
 });
