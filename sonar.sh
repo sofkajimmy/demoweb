310ecb0b1b8db10668da8449f9dc050b756b85a0
Jimmy Muchachasoy <test>
2021-11-03 17:32:16 -0500

    azure variables

--- sonar.sh
+++ sonar.sh
@@ -9,9 +9,9 @@ then
     sed -i "s~_Login_~${login}~" sonar-project.properties
 else
     echo " ---  Azure----"
-    sed -i "s~_Organization_~$(organization)~" sonar-project.properties
-    sed -i "s~_ProjectKey_~$(projectKey)~" sonar-project.properties
-    sed -i "s~_Url_~$(url)~" sonar-project.properties
-    sed -i "s~_Login_~$(login)~" sonar-project.properties
+    sed -i "s~_Organization_~$organization~" sonar-project.properties
+    sed -i "s~_ProjectKey_~$projectKey~" sonar-project.properties
+    sed -i "s~_Url_~$url~" sonar-project.properties
+    sed -i "s~_Login_~$login~" sonar-project.properties
     
 fi
