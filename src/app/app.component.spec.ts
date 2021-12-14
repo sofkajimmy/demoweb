caa38cfc99b26559c25e77a4664c132f3a990601
Jimmy Muchachasoy <test>
2021-12-14 13:50:22 -0500

    test functional

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule,HttpClientModule
       ],
       declarations: [
         AppComponent
@@ -20,10 +22,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Proteccion'`, () => {
+  it(`should have as title 'GB'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Proteccion');
+    expect(app.title).toEqual('GB');
   });
 
   it('should render title', () => {
--- src/app/saludo.service.spec.ts
+++ src/app/saludo.service.spec.ts
@@ -1,16 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
 import { SaludoService } from './saludo.service';
 
 describe('SaludoService', () => {
   let service: SaludoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule,HttpClientModule], 
+      providers: [SaludoService]
+    });
     service = TestBed.inject(SaludoService);
   });
 
   it('should be created', () => {
+    const service: SaludoService = TestBed.get(SaludoService);
     expect(service).toBeTruthy();
   });
 });
